ab533e967c6f4f0c5946a21cd9ab650f127b9fc6
mantey <contactmantey@gmail.com>
2019-01-31 20:23:01 +0000

    undefined method loadRoutesFrom

--- src/ContactFormServiceProvider.php
+++ src/ContactFormServiceProvider.php
@@ -9,7 +9,6 @@
 
 namespace mantey\contactform;
 use Illuminate\Support\ServiceProvider;
-//use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
 class ContactFormServiceProvider extends ServiceProvider {
 
